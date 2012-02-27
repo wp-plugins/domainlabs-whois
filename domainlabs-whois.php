5dde91d0db14f294c6a2ffaf7adab1e1caf1689b
bmericc <bmericc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-27 10:33:11 +0000

    minor update
    git-svn-id: https://plugins.svn.wordpress.org/domainlabs-whois/tags/1.0.1@511047 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ domainlabs-whois.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ * @package Domainlabs_Whois
+ * @version 1.0.1
+ */
+/*
+Plugin Name: Domainlabs Domain Whois
+Plugin URI: http://wordpress.org/extend/plugins/domainlabs-whois/
+Description: DomainLabs Domain Whois Plugin.
+Author: Bahri Meriç CANLI
+Version: 1.0.1
+Author URI: http://www.domainlabs.eu/
+*/
+ 
+if (!class_exists("DomainLabsWhois")) {
+	class DomainLabsWhois {
+
+		function whoisForm($domain="")
+		{
+			
+		$domainwhoisAdminOptions = $this->get_domainwhoisOptions();
+
+		$widget = '
+		<div>
+		<div>Domain whois</div>
+			<form action="'. get_bloginfo('wpurl').$domainwhoisAdminOptions['whois-redirect-url'].'" id="wpwhoisfor" method="post" style="margin: 0px;"> 
+				<input id="wpwhoisdomain" name="domain" type="text" class="whois-text" value="'.$domain.'" /> 
+				<input name="lookup" type="submit" value="Whois" class="whois-submit" />
+			</form> 
+		</div>
+		';
+
+		return $widget;
+		}
+
+		     
+
+		function addHeaderCode() {
+			echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/domainlabs-whois/domainlabs-whois.css" />' . "\n";		
+		}
+		 
+		function widget_Whois($args) {
+		  extract($args);
+		  echo $before_widget;
+		  echo $before_title;
+		  //Whois
+		  echo $after_title;
+		  echo $this->whoisForm();
+		  echo $after_widget;
+		}
+		
+		function control_Whois(){
+    		echo 'This widget is not have a options';
+  		}
+
+/*********************************************************************/  	 		
+	function get_whois_server_detail($domain) {
+		
+		$extension = substr($domain, strrpos($domain, ".")+1);
+		
+		switch ($extension) {
+			case "tr":
+				$server["host"] = "whois.nic.tr";
+				$server["notfound"] = "No match found for";
+				break;
+			case "com":	
+				$server["host"] = "whois.verisign-grs.com";
+				$server["notfound"] = "No match for";
+				break;
+			case "net":	
+				$server["host"] = "whois.verisign-grs.com";
+				$server["notfound"] = "No match for";
+				break;
+			case "name":	
+				$server["host"] = "whois.nic.name";
+				$server["notfound"] = "No match";
+				break;		
+			case "info":	
+				$server["host"] = "whois.afilias.net";
+				$server["notfound"] = "NOT FOUND";
+				break;		
+			case "org":	
+				$server["host"] = "whois.pir.org";
+				$server["notfound"] = "NOT FOUND";
+				break;	
+			case "ru":	
+				$server["host"] = "whois.ripn.net";
+				$server["notfound"] = "No entries found for the selected source";
+				break;	
+			case "su":	
+				$server["host"] = "whois.ripn.net";
+				$server["notfound"] = "No entries found for the selected source";
+				break;					
+			case "tv":	
+				$server["host"] = "tvwhois.verisign-grs.com";
+				$server["notfound"] = "No match for";
+				break;		
+			case "biz":	
+				$server["host"] = "whois.biz";
+				$server["notfound"] = "Not found";
+				break;									
+			case "us":	
+				$server["host"] = "whois.nic.us";
+				$server["notfound"] = "Not found:";
+				break;		
+			case "uk":	
+				$server["host"] = "whois.nic.uk";
+				$server["notfound"] = "No match for";
+				break;	
+			case "ch":	
+				$server["host"] = "whois.nic.ch";
+				$server["notfound"] = "We do not have an entry in our database matching your query.";
+				break;		
+			default:	
+				$server["host"] = "whois.ripe.net";
+				$server["notfound"] = "No entries found";
+				break;		
+		}
+		
+	return 	$server;
+	}
+
+	function whois($url,$domain_ip){
+		$sock = fsockopen($url, 43, $errno, $errstr);
+	    if (!$sock) exit("$errno($errstr)");
+	   	else {
+	     fputs ($sock, $domain_ip."\r\n");
+	     $text = "";
+	     while (!feof($sock))
+	     {
+	       $text .= fgets ($sock, 128)."<br>";
+	     }
+	    fclose ($sock);
+	    $pattern = "|ReferralServer: whois://([^\n<:]+)|i";
+	    preg_match($pattern, $text, $out);
+	    if(!empty($out[1])) return whois($out[1], $domain_ip);
+	    else return $text;
+	    }
+  	}
+  	
+  	
+  	
+ 	function whoisResults($domain) {
+		$serverDetail = $this->get_whois_server_detail($domain);
+		$answer = $this->whois($serverDetail["host"],$domain);
+		if (strpos ($answer, $serverDetail["notfound"])==FALSE){
+				return $answer;	
+		}
+		else return "Domain not found";
+	} 	
+  		
+/*********************************************************************/  	
+
+	function whoisresult_callback($content) {
+		$results = "";
+		if (strpos($content,'%%whoisresults%%')!== false) {
+				$domain = stripslashes(trim($_POST['domain']));
+				if(isset($_POST['lookup'])) {
+					
+					$results .= $this->whoisForm($domain);
+					$results .= '<div class="whois-results"><pre>';
+					$results .= $this->whoisResults($domain);
+					$results .= "</pre></div>";
+					
+				}
+				else $results .= $this->whoisForm();
+			
+		}
+		
+		$content = str_replace('%%whoisresults%%', $results, $content);
+	return $content;
+	}
+
+/*********************************************************************/  		
+  		
+  		function get_domainwhoisOptions() {
+			$domainwhoisAdminOptions = array(
+				'whois-redirect-url' => "/whois"
+				);
+				
+			$domainwhoisOptions = get_option("domainwhoisAdminOptions");
+			if ( !empty($domainwhoisOptions) ) {
+				foreach ( $domainwhoisOptions as $key => $option ) {
+					$domainwhoisAdminOptions[$key] = $option;
+				}
+			}
+			update_option("domainwhoisAdminOptions", $domainwhoisAdminOptions);
+		return $domainwhoisAdminOptions;
+		}
+  		
+  		function print_domainwhoisAdminPage()
+		{
+	
+	
+		$domainwhoisAdminOptions = $this->get_domainwhoisOptions();
+	
+		if (isset($_POST['update_domainwhoisSettings'])) {
+	
+			if (isset($_POST['ll_whois-redirect-url'])) {
+				$domainwhoisAdminOptions['whois-redirect-url'] = $_POST['ll_whois-redirect-url'];
+			}
+			
+			update_option("domainwhoisAdminOptions", $domainwhoisAdminOptions);
+			?>
+	<div class="updated"><p><strong><?php _e("Settings Updated.", "domainwhois");?></strong></p></div>
+<?php
+		}
+	
+?>
+		<div class="wrap">
+		<div id="icon-plugins" class="icon32"><br /></div><h2>DomainLabs Domain Whois Settings</h2>
+		
+		<form method="post" action="<?php echo esc_attr($_SERVER["REQUEST_URI"]); ?>">
+		<input type='hidden' name='option_page' value='general' />
+		<input type="hidden" name="action" value="update" />
+		
+		<table class="form-table">
+		<tr valign="top">
+			<th scope="row"><label for="siteurl">Redirect Address (URL)</label></th>
+			<td><input name="ll_whois-redirect-url" type="text" id="whois-redirect" value="<?php echo esc_attr($domainwhoisAdminOptions['whois-redirect-url']); ?>" class="regular-text code" />
+			<span class="description">without <?php  echo get_bloginfo('wpurl'); ?> </span>	</td>
+		</tr>
+		</table>
+		
+		<p class="submit"><input type="submit" name="update_domainwhoisSettings" id="submit" class="button-primary" value="Save Changes"  /></p></form>
+		
+		
+		
+		</form>
+		</div>
+<?php
+
+		}
+		
+/*********************************************************************/		
+  		
+  		function adminMenu() {
+ 		    add_options_page("DomainLabs Domain Whois", "Domain Whois", 1, "Domain-Whois", array(&$this, "print_domainwhoisAdminPage"));
+  		}
+  		
+	}
+}  //End Class DomainLabsWhois
+
+
+if (class_exists("DomainLabsWhois")) {
+	$dl_domainLabsWhois = new DomainLabsWhois();
+}
+ 
+function whois_init() {
+	global $dl_domainLabsWhois;
+    register_sidebar_widget(__('Domain Whois'), array(&$dl_domainLabsWhois, 'widget_Whois'));
+    register_widget_control(__('Domain Whois'), array(&$dl_domainLabsWhois, 'control_Whois'));
+}
+
+if (isset($dl_domainLabsWhois)) {
+	add_action('wp_head', array(&$dl_domainLabsWhois, 'addHeaderCode'), 1);
+	add_action('admin_menu', array(&$dl_domainLabsWhois, 'adminMenu'), 1);
+	add_action('plugins_loaded', 'whois_init');
+	add_filter('the_content',array(&$dl_domainLabsWhois, 'whoisresult_callback'), 7);
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== DomainLabs Whois ===
+Contributors: bmericc
+Donate link: http://links.bahri.info/donation
+Tags: domain, whois, widget
+Requires at least: 3.0
+Tested up to: 3.1
+Stable tag: 1.0.1
+
+DomainLabs Domain Whois Plugin for Wordpress 
+
+== Description ==
+
+DomainLabs whois plugin; domain and Ip address whois lookup allows you on with Wordpress
+
+== Installation ==
+
+1. Upload `domainlabs-whois.zip` to the `/wp-content/plugins/` directory
+1. Extract to this directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add to domain whois widget to your templates
+1. Create to page or entry for whois result pages
+1. Add to %%whoisresults%% keyword in created page or entry
+1. Set to whois result page url Settings -> Domain Whois 
+
+== Screenshots ==
+
+1. This screen shot Settings -> Domain Whois
+2. This screen shot widget
+3. This screen shot whois results page
+
+== Frequently Asked Questions ==
+
+Nobody asked anything yet
+
+== Changelog ==
+
+1.0.1
+
+* .us, .uk, .ch support has been added
+
+1.0
+
+* First version published
+
+== Upgrade Notice ==
+
+* An optional update. You can look at Changelog
